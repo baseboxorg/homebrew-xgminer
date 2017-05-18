6733a973f65d8833e0d649919e0fb53188a4d020
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2017-05-18 09:00:17 +0800

    Update dualminer-cgminer.rb

--- dualminer-cgminer.rb
+++ dualminer-cgminer.rb
@@ -3,8 +3,8 @@ require 'formula'
 class DualminerCgminer < Formula
   homepage 'https://github.com/baseboxorg/dualminer-cgminer'
   head 'https://github.com/baseboxorg/dualminer-cgminer.git', :branch => 'master'
-  url 'https://github.com/baseboxorg/dualminer-cgminer/archive/1.0.0.tar.gz'
-  sha256 '3cea48e26bab0ae99976818cbb9812692cb4030db8307cdc9f13505f094a39c5'
+  url 'https://github.com/baseboxorg/dualminer-cgminer/archive/1.0.1.tar.gz'
+  sha256 '8da024f4243e8305ecde39dcad3929351d606dd3f58c13fbff317cd6488be5db'
 
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
